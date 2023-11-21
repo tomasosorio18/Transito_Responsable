eac23de7f37e1785ac4ecd472e206885f18ba92c
Tomas Osorio <tomas.osorio@sansano.usm.cl>
2023-11-21 18:29:54 -0300

    rollback

---
+++ transitMobile/app/src/main/java/com/jtn/transitmobile/Vistas_pruebas/SacarPartes/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.jtn.transitmobile.Vistas_pruebas.sacar_partes
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "Acá se sacarán los partes"
+    }
+    val text: LiveData<String> = _text
+}
