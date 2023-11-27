4f200dcebb0e4f7c21de518697e13a65dcce1e9b
Nicolasnanjari <102549955+Nicolasnanjari@users.noreply.github.com>
2023-11-27 16:19:35 -0300

    Delete transitMobile directory

--- transitMobile/app/src/main/java/com/jtn/transitmobile/Commons/Persona.kt
+++
@@ -1,9 +0,0 @@
-package com.jtn.transitmobile.Commons
-
-data class Persona(
-    val rut: String,
-    val nombre: String,
-    val apellido_paterno: String,
-    val apellido_materno: String,
-    val domicilio:String,
-    val vehiculo_patente:String)
